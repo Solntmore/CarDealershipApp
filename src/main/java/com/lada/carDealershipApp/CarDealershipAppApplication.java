eff68121f4f4264a889e06761331868703ed5884
Solntsev Maksim <solntsev1099@gmail.com>
2023-03-12 15:22:57 +0300

    Initial commit

---
+++ src/main/java/com/lada/carDealershipApp/CarDealershipAppApplication.java
@@ -0,0 +1,13 @@
+package com.lada.carDealershipApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipAppApplication.class, args);
+	}
+
+}
